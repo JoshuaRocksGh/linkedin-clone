250df3d3c472ac7fbe8f15eabadd290568f5f8de
josh.tetteh01 <josh.tetteh01@gmail.com>
2022-05-04 15:46:30 +0000

    delete feedback

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -5,11 +5,17 @@ import FeedbackData from './data/FeedbackData';
 
 function App() {
 	const [feedback, setFeedBack] = useState(FeedbackData);
+	const deleteFeedback = (id) => {
+		// console.log('App ==>', id);
+		if (window.confirm('Are you sure you want to delete?')) {
+			setFeedBack(feedback.filter((item) => item.id !== id));
+		}
+	};
 	return (
 		<>
 			<Header />
 			<div className='container'>
-				<FeedbackList feedback={feedback} />
+				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
 			</div>
 		</>
 	);
